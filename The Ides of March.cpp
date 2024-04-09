c31a30c7e3f10d95ef6f21adff1ac990104bf5e0
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-04-10 00:35:59 +0600

    Add files via upload

---
+++ The Ides of March.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+int main() {
+	int a;
+	cin>>a;
+	if(a==15)
+	{
+	    cout<<"YES"<<endl;
+	}
+	else
+	{
+	    cout<<"No"<<endl;
+	}
+
+}
